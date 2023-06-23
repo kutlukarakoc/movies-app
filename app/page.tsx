de15d61132f12008aab84d9c5bdc04a891256147
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-24 01:32:16 +0300

    added newmovies page to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/home-components/hero'
 import Brands from '@/components/home-components/brands'
+import NewMovies from '@/components/home-components/new-movies'
 
 export default function Home() {
   return (
     <main className='h-full text-secondary'>
       <Hero />
       <Brands />
+      <NewMovies />
     </main>
   )
 }
