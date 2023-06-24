4e5f904430bbce1db05bf00c8641d3139115dce9
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-24 03:57:04 +0300

    added upcoming movies section and its loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import Hero from '@/components/home-components/hero'
 import Brands from '@/components/home-components/brands'
-import NewMovies from '@/components/home-components/new-movies'
+import UpcomingMovies from '@/components/home-components/upcomings/movies'
 
 export default function Home() {
   return (
     <main className='h-full text-secondary'>
       <Hero />
       <Brands />
-      <NewMovies />
+      <UpcomingMovies />
     </main>
   )
 }
---
+++ components/home-components/upcomings/loading.tsx
@@ -0,0 +1,19 @@
+const UpcomingMoviesLoading: React.FC = () => {
+  return (
+    <section className='section-container animate-pulse'>
+      <div className='flex justify-between items-center mb-4'>
+        <div className='w-40 h-7 rounded-md bg-zambezi'></div>
+        <h4 className='text-xl text-secondary rounded-md bg-zambezi'></h4>
+        <div className='w-16 h-4 rounded-md bg-zambezi'></div>
+      </div>
+      <div className="overflow-hidden flex gap-5">
+        <span className='flex-shrink-0 w-[240px] h-[400px] rounded-md bg-zambezi'></span>
+        <span className='flex-shrink-0 w-[240px] h-[400px] rounded-md bg-zambezi'></span>
+        <span className='flex-shrink-0 w-[240px] h-[400px] rounded-md bg-zambezi'></span>
+        <span className='flex-shrink-0 w-[240px] h-[400px] rounded-md bg-zambezi'></span>
+      </div>
+    </section>
+  )
+}
+
+export default UpcomingMoviesLoading
