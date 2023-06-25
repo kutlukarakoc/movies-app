01fef9f45b68a68e416eb39c8b9d928841fa2b94
Kutlu Karakoc <karakockutlu@gmail.com>
2023-06-25 17:45:27 +0300

    removed main element

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import WhyChoose from '@/components/home-components/why-choose'
 
 export default function Home() {
   return (
-    <main className='h-full text-secondary'>
+    <>
       <Hero />
       <Brands />
       <MovieSlide url='https://api.themoviedb.org/3/movie/upcoming' reqKey='upcomingMovies' title='Upcoming Movies' />
@@ -14,7 +14,6 @@ export default function Home() {
       <TvSeriesSlide url='https://api.themoviedb.org/3/tv/on_the_air' reqKey='upcomingTvSeries' title='Upcoming Tv Series' className='mb-10' />
       <TvSeriesSlide url='https://api.themoviedb.org/3/tv/top_rated' reqKey='topRatedTvSeries' title='Top Rated Tv Series' className='mb-24' />
       <WhyChoose />
-      <div className='mt-44'>test</div>
-    </main>
+    </>
   )
 }
