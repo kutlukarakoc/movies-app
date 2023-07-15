2eb21e841fd1e8084626dd5b463cdbbf44ecc316
Kutlu Karakoc <karakockutlu@gmail.com>
2023-07-15 19:24:45 +0300

    added title loading

--- components/page-components/movie/cast/loading.tsx
+++ components/page-components/movie/cast/loading.tsx
@@ -1,6 +1,7 @@
 const CastLoading: React.FC = () => {
   return (
     <section className='section-container animate-pulse'>
+      <div className='w-28 h-10 mb-5 rounded-md bg-zambezi'></div>
       <div className='flex justify-between items-center mb-4'>
         {Array.from({ length: 7 }).map((_, index: number) => (
           <div key={index} className='w-40 h-52 flex flex-col items-center'>
