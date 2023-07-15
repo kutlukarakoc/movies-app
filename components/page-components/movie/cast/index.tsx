95077625aff9973cfc9b42467c75c9cb4372e914
Kutlu Karakoc <karakockutlu@gmail.com>
2023-07-15 19:10:20 +0300

    display icon if person doesn't have picture

--- components/page-components/movie/cast/index.tsx
+++ components/page-components/movie/cast/index.tsx
@@ -3,6 +3,8 @@
 import CastLoading from './loading'
 import Image from 'next/image'
 import Link from 'next/link'
+import { SlUserFemale } from 'react-icons/sl'
+import { UserIcon } from '@heroicons/react/24/outline'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { useQuery } from '@tanstack/react-query'
 import { axiosGet } from '@/public/utils/fetch'
@@ -41,29 +43,29 @@ const Cast: React.FC<{ movieId: string }> = ({ movieId }) => {
           style={{ marginLeft: '-8px' }}
         >
           {data.map((person: CastProps) => (
-            <>
-              {person.profile_path &&
-                <SwiperSlide key={person.id} style={{ width: '160px', height: '208px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                  <Link href={`/person/${person.cast_id}`} className='block relative w-36 h-36'>
-                    <Image
-                      src={`https://image.tmdb.org/t/p/w500${person.profile_path}`}
-                      alt={person.name}
-                      fill
-                      className='rounded-full object-cover bg-zambezi'
-                      unoptimized
-                    />
-                  </Link>
-                  <h6 className='text-sm mt-2 text-center'>{person.name}</h6>
-                  <p className='text-zambezi text-xs text-center'>{person.character}</p>
-                </SwiperSlide>
-              }
-            </>
+            <SwiperSlide key={person.id} style={{ width: '160px', height: '208px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <Link href={`/person/${person.cast_id}`} className='block relative w-36 h-36'>
+                {person.profile_path ?
+                  <Image
+                    src={`https://image.tmdb.org/t/p/w500${person.profile_path}`}
+                    alt={person.name}
+                    fill
+                    className='rounded-full object-cover bg-zambezi'
+                    unoptimized
+                  /> :
+                  <div className='w-36 h-36 flex rounded-full justify-center items-center bg-zambezi'>
+                    {person.gender === 1 ? <SlUserFemale size={44} /> : <UserIcon className='w-12 h-12' />}
+                  </div>
+                }
+              </Link>
+              <h6 className='text-sm mt-2 text-center'>{person.name}</h6>
+              <p className='text-zambezi text-xs text-center'>{person.character}</p>
+            </SwiperSlide>
           ))}
         </Swiper>
       </section>
     )
   }
-
   return null
 }
 
