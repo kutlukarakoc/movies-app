e46a4a8f078f26a6f10495f183da79b66ff14a84
Kutlu Karakoc <karakockutlu@gmail.com>
2023-07-16 16:21:02 +0300

    changed name

--- app/movies/[slug]/page.tsx
+++ app/movies/[slug]/page.tsx
@@ -1,9 +1,9 @@
 import MoviesWithSlug from '@/components/page-components/movies/slugs'
 
-const PopularPage: React.FC<{ params: { slug: string } }> = ({ params }) => {
+const MoviesSlugPage: React.FC<{ params: { slug: string } }> = ({ params }) => {
   return (
     <MoviesWithSlug slug={params.slug} />
   )
 }
 
-export default PopularPage
+export default MoviesSlugPage
