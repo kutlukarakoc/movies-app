c318618957979b01d202c7fb7ad9610ea9e689a4
Kutlu Karakoc <karakockutlu@gmail.com>
2023-07-16 16:49:11 +0300

    added key

--- components/page-components/movie/reviews/loading.tsx
+++ components/page-components/movie/reviews/loading.tsx
@@ -4,7 +4,7 @@ const ReviewsLoading: React.FC = () => {
       <div className='w-32 h-10 mb-5 m-auto rounded-md bg-zambezi'></div>
       <article className='flex flex-col items-center justify-center gap-y-6'>
         {Array.from({ length: 4 }).map((_, index: number) => (
-          <div className='flex items-start justify-center gap-8'>
+          <div key={index} className='flex items-start justify-center gap-8'>
             <div className='w-16 h-16 rounded-full bg-zambezi'></div>
             <div className='p-3 w-[550px] min-h-[120px] rounded-md bg-zambezi'></div>
           </div>
