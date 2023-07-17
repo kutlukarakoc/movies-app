4f874e53f14d8f17a69a6b3e3f8995e2e52d4964
Kutlu Karakoc <karakockutlu@gmail.com>
2023-07-18 00:16:40 +0300

    added card

--- components/page-components/movies/slugs/index.tsx
+++ components/page-components/movies/slugs/index.tsx
@@ -1,9 +1,8 @@
 'use client'
 
+import Card from '@/components/movie-serie-card'
 import MoviesWithSlugLoading from './loading'
 import Pagination from './pagination'
-import Image from 'next/image'
-import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
 import { useQuery } from '@tanstack/react-query'
 import { axiosGet } from '@/public/utils/fetch'
@@ -40,7 +39,7 @@ const MoviesWithSlug: React.FC<{ slug: string }> = ({ slug }) => {
   return (
     <section className='section-container mt-14'>
       <h1 className='capitalize text-xl md:text-2xl mb-10 text-center md:text-left'>{slug} Movies</h1>
-      <h5 className='text-sm md:text-md text-center md:text-end mb-2'>Showing
+      <h5 className='text-sm md:text-md text-center md:text-start mb-2'>Showing
         <span className='ml-1'>
           {+page === 1 ? page : page + 1} - {+page * 20}
           <span className='text-xs mx-1'>of</span>
@@ -49,17 +48,12 @@ const MoviesWithSlug: React.FC<{ slug: string }> = ({ slug }) => {
       </h5>
       <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 place-items-center md:place-items-stretch gap-y-14 gap-x-10'>
         {data?.results.map((movie: Movie) => (
-          <div key={movie.id} className='w-full max-w-[275px]'>
-            <Link href={`/movie/${movie.id}`}>
-              <Image
-                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                alt={movie.title}
-                width={288}
-                height={375}
-                className='rounded-sm h-full max-h-[375px]'
-                unoptimized
-              />
-            </Link>
+          <div key={movie.id} className='w-full max-w-[250px]'>
+            <Card
+              url={`/movie/${movie.id}`}
+              imgSrc={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+              alt={movie.title}
+            />
             <div className='flex justify-between items-end -mb-1'>
               <h5 className='text-sm md:text-md text-secondary mt-1 truncate'>{movie.title}</h5>
               <div className='flex items-center gap-1'>
