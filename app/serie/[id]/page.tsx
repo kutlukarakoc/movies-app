066866e86d05ccbfdbe99d080de3ca1814e64250
Kutlu Karakoc <karakockutlu@gmail.com>
2023-07-20 22:48:29 +0300

    changed request url

--- app/serie/[id]/page.tsx
+++ app/serie/[id]/page.tsx
@@ -1,5 +1,6 @@
 import Hero from '@/components/page-components/serie/hero'
 import Seasons from '@/components/page-components/serie/seasons'
+import Cast from '@/components/detail-cast'
 
 const Serie = ({ params }: { params: { id: string } }) => {
 
@@ -7,6 +8,7 @@ const Serie = ({ params }: { params: { id: string } }) => {
     <>
       <Hero serieId={params.id} />
       <Seasons serieId={params.id} />
+      <Cast id={params.id} reqUrl={`/tv/${params.id}/aggregate_credits`} />
     </>
   )
 }
