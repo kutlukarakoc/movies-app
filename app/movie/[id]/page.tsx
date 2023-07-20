ba9d749e36d57c6b0ec9486c22d7caed795cff34
Kutlu Karakoc <karakockutlu@gmail.com>
2023-07-20 23:01:55 +0300

    changed similars with detail component

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -2,7 +2,7 @@ import Hero from '@/components/page-components/movie/hero'
 import Trailers from '@/components/page-components/movie/trailers'
 import Cast from '@/components/detail-cast'
 import Reviews from '@/components/page-components/movie/reviews'
-import Similars from '@/components/page-components/movie/similar'
+import Similars from '@/components/detail-similar'
 
 const Movie = ({ params }: { params: { id: string } }) => {
 
@@ -12,7 +12,7 @@ const Movie = ({ params }: { params: { id: string } }) => {
       <Trailers movieId={params.id} />
       <Cast id={params.id} reqUrl={`/movie/${params.id}/credits`} />
       <Reviews movieId={params.id} />
-      <Similars movieId={params.id} />
+      <Similars title='Movies' id={params.id} reqUrl={`/movie/${params.id}/similar`} />
     </>
   )
 }
