72afb876d8c7a8ab86f7fe8244687b84ae72476b
Kutlu Karakoc <karakockutlu@gmail.com>
2023-07-20 22:53:21 +0300

    added cast as a detail component

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,6 +1,6 @@
 import Hero from '@/components/page-components/movie/hero'
 import Trailers from '@/components/page-components/movie/trailers'
-import Cast from '@/components/page-components/movie/cast'
+import Cast from '@/components/detail-cast'
 import Reviews from '@/components/page-components/movie/reviews'
 import Similars from '@/components/page-components/movie/similar'
 
@@ -10,7 +10,7 @@ const Movie = ({ params }: { params: { id: string } }) => {
     <>
       <Hero movieId={params.id} />
       <Trailers movieId={params.id} />
-      <Cast movieId={params.id} />
+      <Cast id={params.id} reqUrl={`/movie/${params.id}/credits`} />
       <Reviews movieId={params.id} />
       <Similars movieId={params.id} />
     </>
