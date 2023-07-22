4117b558fb204803257da87b350c3d47fb8edcbe
Kutlu Karakoc <karakockutlu@gmail.com>
2023-07-22 16:05:14 +0300

    added extra control and added html for error

--- components/page-components/series/slugs/index.tsx
+++ components/page-components/series/slugs/index.tsx
@@ -39,28 +39,36 @@ const SeriesWithSlug: React.FC<{ slug: string }> = ({ slug }) => {
     return <SeriesWithSlugLoading />
   }
 
+  if (!error && data && data.results.length) {
+    return (
+      <section className='section-container mt-14'>
+        <h1 className='capitalize text-xl md:text-2xl mb-10 text-center md:text-left'>{title} TV Series</h1>
+        <h5 className='text-sm md:text-md text-center md:text-end mb-2'>Showing
+          <span className='ml-1'>
+            {+page === 1 ? page : page + 1} - {+page * 20}
+            <span className='text-xs mx-1'>of</span>
+            {data?.total_results}
+          </span>
+        </h5>
+        <GridCards>
+          {data?.results.map((serie: Serie) => (
+            <Card
+              key={serie.id}
+              url={`/serie/${serie.id}`}
+              imgSrc={`https://image.tmdb.org/t/p/w500${serie.poster_path}`}
+              title={serie.name}
+              averageVote={serie.vote_average}
+            />
+          ))}
+        </GridCards>
+        <Pagination page={page} totalPages={data?.total_pages} />
+      </section>
+    )
+  }
+
   return (
     <section className='section-container mt-14'>
-      <h1 className='capitalize text-xl md:text-2xl mb-10 text-center md:text-left'>{title} TV Series</h1>
-      <h5 className='text-sm md:text-md text-center md:text-end mb-2'>Showing
-        <span className='ml-1'>
-          {+page === 1 ? page : page + 1} - {+page * 20}
-          <span className='text-xs mx-1'>of</span>
-          {data?.total_results}
-        </span>
-      </h5>
-      <GridCards>
-        {data?.results.map((serie: Serie) => (
-          <Card
-            key={serie.id}
-            url={`/serie/${serie.id}`}
-            imgSrc={`https://image.tmdb.org/t/p/w500${serie.poster_path}`}
-            title={serie.name}
-            averageVote={serie.vote_average}
-          />
-        ))}
-      </GridCards>
-      <Pagination page={page} totalPages={data?.total_pages} />
+      <h2 className='text-center text-2xl'>Something went wrong. Please try again later.</h2>
     </section>
   )
 }
