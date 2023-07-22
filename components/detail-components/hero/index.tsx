4cede55d9464d805c48ab9f6a059b3b3014d3588
Kutlu Karakoc <karakockutlu@gmail.com>
2023-07-22 15:55:42 +0300

    added style for when there is no backdrop path

--- components/detail-components/hero/index.tsx
+++ components/detail-components/hero/index.tsx
@@ -13,16 +13,18 @@ type Hero = {
 const Hero: React.FC<Hero> = ({ children, backdropPath, title, overview, status, date }) => {
   return (
     <section className='group w-full h-[450px] md:h-[625px] lg:h-[725px] relative mb-28'>
-      <Image
-        src={`https://image.tmdb.org/t/p/original${backdropPath}`}
-        alt={title}
-        className='object-cover opacity-60 sm:opacity-100 sm:group-hover:opacity-10 transition-opacity'
-        fill
-        sizes='100vw'
-        quality={100}
-      />
-      <div className='w-full absolute z-10 text-center md:text-left bottom-5 md:bottom-20 left-1/2 -translate-x-1/2 md:-translate-x-0 md:left-14 max-w-[85%] md:max-w-[75%] lg:max-w-screen-md sm:opacity-0 sm:group-hover:opacity-100 transition-opacity'>
-        <div className='flex items-center justify-center md:justify-start gap-8  mb-3'>
+      {backdropPath &&
+        <Image
+          src={`https://image.tmdb.org/t/p/original${backdropPath}`}
+          alt={title}
+          className='object-cover opacity-60 sm:opacity-100 sm:group-hover:opacity-10 transition-opacity'
+          fill
+          sizes='100vw'
+          quality={100}
+        />
+      }
+      <div className={`w-full absolute z-10 text-center md:text-left bottom-5 md:bottom-20 left-1/2 -translate-x-1/2 md:-translate-x-0 md:left-14 max-w-[85%] md:max-w-[75%] lg:max-w-screen-md ${backdropPath ? 'sm:opacity-0 sm:group-hover:opacity-100 transition-opacity' : ''}`}>
+        <div className='flex items-center justify-center md:justify-start gap-8 mb-3'>
           <h1 className='text-secondary text-xl md:text-2xl lg:text-3xl font-semibold'>
             {title}
           </h1>
