f0a4e85f7cda795fd7e4a0e89f2b451366d2f8b5
Kutlu Karakoc <karakockutlu@gmail.com>
2023-07-22 16:01:17 +0300

    added extra control and error html

--- components/page-components/search-results/index.tsx
+++ components/page-components/search-results/index.tsx
@@ -38,28 +38,36 @@ const SearchResults: React.FC = () => {
     return <ResultsLoading />
   }
 
+  if (!error && data && data.results.length) {
+    return (
+      <section className='section-container mt-14'>
+        <h1 className='capitalize text-xl md:text-2xl mb-10 text-center md:text-left'>Showing Results of '{searchQuery}'</h1>
+        <h5 className='text-sm md:text-md text-center md:text-left mb-2'>Showing
+          <span className='ml-1'>
+            {+page === 1 ? page : page + 1} - {+page * 20}
+            <span className='text-xs mx-1'>of</span>
+            {data?.total_results}
+          </span>
+        </h5>
+        <GridCards>
+          {data?.results.map((movie: Movie) => (
+            <Card
+              key={movie.id}
+              url={`/movie/${movie.id}`}
+              imgSrc={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              title={movie.title}
+              averageVote={movie.vote_average}
+            />
+          ))}
+        </GridCards>
+        <Pagination page={page} totalPages={data?.total_pages} />
+      </section>
+    )
+  }
+
   return (
     <section className='section-container mt-14'>
-      <h1 className='capitalize text-xl md:text-2xl mb-10 text-center md:text-left'>Showing Results of '{searchQuery}'</h1>
-      <h5 className='text-sm md:text-md text-center md:text-left mb-2'>Showing
-        <span className='ml-1'>
-          {+page === 1 ? page : page + 1} - {+page * 20}
-          <span className='text-xs mx-1'>of</span>
-          {data?.total_results}
-        </span>
-      </h5>
-      <GridCards>
-        {data?.results.map((movie: Movie) => (
-          <Card
-            key={movie.id}
-            url={`/movie/${movie.id}`}
-            imgSrc={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            title={movie.title}
-            averageVote={movie.vote_average}
-          />
-        ))}
-      </GridCards>
-      <Pagination page={page} totalPages={data?.total_pages} />
+      <h2 className='text-center text-2xl'>No results found of '{searchQuery}'</h2>
     </section>
   )
 }
