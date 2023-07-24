2bc92b4bc73f1398a1e36b15c7579c7dd32c8f1d
Kutlu Karakoc <karakockutlu@gmail.com>
2023-07-24 22:03:14 +0300

    added persons acted medias component

--- app/person/[id]/page.tsx
+++ app/person/[id]/page.tsx
@@ -1,8 +1,14 @@
 import PersonHero from '@/components/page-components/person/hero'
+import ActedMovies from '@/components/page-components/person/acted/actedMovies'
+import ActedSeries from '@/components/page-components/person/acted/actedSeries'
 
 const Person = ({ params }: { params: { id: string } }) => {
   return (
-    <PersonHero personId={params.id} />
+    <>
+      <PersonHero personId={params.id} />
+      <ActedMovies personId={params.id} />
+      <ActedSeries personId={params.id} />
+    </>
   )
 }
 
