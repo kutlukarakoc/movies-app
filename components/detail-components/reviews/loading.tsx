e83d5afdff1709083a15df49dd3b0c273fb1ca63
Kutlu Karakoc <karakockutlu@gmail.com>
2023-07-26 20:38:30 +0300

    added loading style for mobile

--- components/detail-components/reviews/loading.tsx
+++ components/detail-components/reviews/loading.tsx
@@ -5,8 +5,8 @@ const ReviewsLoading: React.FC = () => {
       <article className='flex flex-col items-center justify-center gap-y-6'>
         {Array.from({ length: 4 }).map((_, index: number) => (
           <div key={index} className='flex items-start justify-center gap-8'>
-            <div className='w-16 h-16 rounded-full bg-zambezi'></div>
-            <div className='p-3 w-[550px] min-h-[120px] rounded-md bg-zambezi'></div>
+            <div className='min-w-[40px] min-h-[40px] w-10 h-10 sm:w-16 sm:h-16 rounded-full bg-zambezi'></div>
+            <div className='p-3 min-w-[221px] max-w-[271px] w-[271px] sm:max-w-[550px] sm:w-[550px] min-h-[120px] rounded-md bg-zambezi'></div>
           </div>
         ))}
       </article>
