91e5287e20e98ec9b0b5e538a9f9629281a5dd86
Kutlu Karakoc <karakockutlu@gmail.com>
2023-07-26 20:31:29 +0300

    mobile loading bug fix

--- components/detail-components/cast/loading.tsx
+++ components/detail-components/cast/loading.tsx
@@ -2,9 +2,9 @@ const CastLoading: React.FC = () => {
   return (
     <section className='section-container animate-pulse'>
       <div className='w-28 h-10 mb-5 rounded-md bg-zambezi'></div>
-      <div className='flex justify-between items-center mb-4'>
+      <div className="overflow-hidden flex gap-5">
         {Array.from({ length: 5 }).map((_, index: number) => (
-          <div key={index} className='w-56 h-72 rounded-md bg-zambezi'></div>
+          <span key={index} className='flex-shrink-0 w-56 h-72 rounded-md bg-zambezi'></span>
         ))}
       </div>
     </section>
