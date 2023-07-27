5865215fbed61847527e75bf3644f203cec96ec1
Kutlu Karakoc <karakockutlu@gmail.com>
2023-07-28 00:04:33 +0300

    get input values and set to form state

--- components/auth/sign-in/index.tsx
+++ components/auth/sign-in/index.tsx
@@ -1,19 +1,54 @@
-import Modal from '@/components/modal'
+'use client'
+
+import { useState, FormEvent } from 'react'
+
+interface SignInForm {
+  email: string
+  password: string
+}
 
 const SignIn: React.FC<{ toggleBetweenModals: () => void }> = ({ toggleBetweenModals }) => {
 
+  const [signInForm, setSignInForm] = useState<SignInForm>({
+    email: '',
+    password: ''
+  })
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('signin', signInForm)
+  }
+
   return (
-    <form action='' className='w-full h-full px-6 py-10 flex flex-col justify-center items-center absolute top-0 left-0'>
+    <form className='w-full h-full px-6 py-10 flex flex-col justify-center items-center absolute top-0 left-0' onSubmit={handleSubmit}>
 
       <h3 className='text-center text-xl text-secondary'>Sign In</h3>
 
       <div className='mb-4 w-full'>
-        <label htmlFor='email' className='text-xs text-zambezi mb-2'>Email</label>
-        <input type='email' name='email' id='email' placeholder='Enter your email' className='bg-zambezi text-secondary placeholder:text-secondary rounded-sm h-10 w-full px-4 text-xs' />
+        <label htmlFor='sign-in-email' className='text-xs text-zambezi mb-2'>Email</label>
+        <input
+          type='email'
+          name='email'
+          id='sign-in-email'
+          placeholder='Enter your email'
+          className='bg-zambezi text-secondary placeholder:text-secondary rounded-sm h-10 w-full px-4 text-xs'
+          autoComplete='off'
+          value={signInForm.email}
+          onChange={event => setSignInForm({...signInForm, [event.target.name]: event.target.value})}
+        />
       </div>
       <div className='mb-8 w-full'>
-        <label htmlFor='password' className='text-xs text-zambezi mb-2'>Password</label>
-        <input type='password' name='password' id='password' placeholder='Enter your password' className='bg-zambezi text-secondary placeholder:text-secondary rounded-sm h-10 w-full px-4 text-xs' />
+        <label htmlFor='sign-in-password' className='text-xs text-zambezi mb-2'>Password</label>
+        <input
+          type='password'
+          name='password'
+          id='sign-in-password'
+          placeholder='Enter your password'
+          className='bg-zambezi text-secondary placeholder:text-secondary rounded-sm h-10 w-full px-4 text-xs'
+          autoComplete='off'
+          value={signInForm.password}
+          onChange={event => setSignInForm({...signInForm, [event.target.name]: event.target.value})}
+        />
       </div>
 
       <button type='submit' className='text-secondary bg-accent hover:bg-accent-light rounded-sm w-full h-10 mb-6'>Sign Up</button>
--- components/auth/sign-up/index.tsx
+++ components/auth/sign-up/index.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import Modal from '@/components/modal'
-import { useState } from 'react'
+import { useState, FormEvent } from 'react'
 
 interface SignUpForm {
   email: string
@@ -17,22 +16,54 @@ const SignUp: React.FC<{ toggleBetweenModals: () => void }> = ({ toggleBetweenMo
     password: ''
   })
 
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('signup', signUpForm)
+  }
+
   return (
-    <form action='' className='w-full h-full px-6 py-10 flex flex-col justify-center items-center absolute top-0 left-0'>
+    <form className='w-full h-full px-6 py-10 flex flex-col justify-center items-center absolute top-0 left-0' onSubmit={handleSubmit}>
 
       <h3 className='text-center text-xl text-secondary'>Create An Account</h3>
 
       <div className='mb-4 w-full'>
-        <label htmlFor='email' className='text-xs text-zambezi mb-2'>Email</label>
-        <input type='email' name='email' id='email' placeholder='Enter your email' className='bg-zambezi text-secondary placeholder:text-secondary rounded-sm h-10 w-full px-4 text-xs' />
+        <label htmlFor='sign-up-email' className='text-xs text-zambezi mb-2'>Email</label>
+        <input
+          type='email'
+          name='email'
+          id='sign-up-email'
+          placeholder='Enter your email'
+          className='bg-zambezi text-secondary placeholder:text-secondary rounded-sm h-10 w-full px-4 text-xs'
+          autoComplete='off'
+          value={signUpForm.email}
+          onChange={event => setSignUpForm({ ...signUpForm, [event.target.name]: event.target.value })}
+        />
       </div>
       <div className='mb-4 w-full'>
-        <label htmlFor='username' className='text-xs text-zambezi mb-2'>Username</label>
-        <input type='text' name='username' id='username' placeholder='Enter your username' className='bg-zambezi text-secondary placeholder:text-secondary rounded-sm h-10 w-full px-4 text-xs' />
+        <label htmlFor='sign-up-username' className='text-xs text-zambezi mb-2'>Username</label>
+        <input
+          type='text'
+          name='username'
+          id='sign-up-username'
+          placeholder='Enter your username'
+          className='bg-zambezi text-secondary placeholder:text-secondary rounded-sm h-10 w-full px-4 text-xs'
+          autoComplete='off'
+          value={signUpForm.username}
+          onChange={event => setSignUpForm({ ...signUpForm, [event.target.name]: event.target.value })}
+        />
       </div>
       <div className='mb-8 w-full'>
-        <label htmlFor='password' className='text-xs text-zambezi mb-2'>Password</label>
-        <input type='password' name='password' id='password' placeholder='Enter your password' className='bg-zambezi text-secondary placeholder:text-secondary rounded-sm h-10 w-full px-4 text-xs' />
+        <label htmlFor='sign-up-password' className='text-xs text-zambezi mb-2'>Password</label>
+        <input
+          type='password'
+          name='password'
+          id='sign-up-password'
+          placeholder='Enter your password'
+          className='bg-zambezi text-secondary placeholder:text-secondary rounded-sm h-10 w-full px-4 text-xs'
+          autoComplete='off'
+          value={signUpForm.password}
+          onChange={event => setSignUpForm({ ...signUpForm, [event.target.name]: event.target.value })}
+        />
       </div>
 
       <button type='submit' className='text-secondary bg-accent hover:bg-accent-light rounded-sm w-full h-10 mb-6'>Sign Up</button>
